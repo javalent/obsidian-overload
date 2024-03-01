23b95541877edd69dafdcc09942ea279189db892
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2024-03-01 15:08:50 -0500

    fix: Remove FS

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 import type DiceRoller from "obsidian-dice-roller";
-import type { StatblockAPI } from "fantasy-statblocks";
 import { DataviewAPI, FullIndex } from "obsidian-dataview";
 import { LeafletAPI } from "obsidian-leaflet";
 
 export interface Plugins {
     "obsidian-dice-roller": DiceRoller;
-    "obsidian-5e-statblocks": StatblockAPI;
     "obsidian-leaflet-plugin": LeafletAPI;
     dataview: { api: DataviewAPI; index: FullIndex };
 }
 
 export * from "obsidian-dice-roller";
-export * from "fantasy-statblocks";
 export * from "obsidian-leaflet";
 export * from "initiative-tracker";
